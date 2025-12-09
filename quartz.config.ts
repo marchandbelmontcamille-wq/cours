904d49f596d1c88fb268a4f962ab78f4f8f42491
camille.marchandbelmont <camille.marchandbelmont@monlycee.net>
2025-12-09 15:11:33 +0100

    Quartz sync: Dec 9, 2025, 3:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,12 +56,6 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-      CreatedModifiedDate({
-        showCreatedDate: true,
-        createdDateFormat: "YYYY-MM-DD HH:mm",
-        showModifiedDate: true,
-        modifiedDateFormat: "YYYY-MM-DD HH:mm",
-      }),
       Plugin.HardLineBreaks(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
